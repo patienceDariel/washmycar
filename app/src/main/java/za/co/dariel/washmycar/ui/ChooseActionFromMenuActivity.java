e7272c6761899b09f2c7fa005cb46a0469f2854a
Patience Mpofu <patience.mpofu@headoffice.dariel-solutions>
2018-02-08 10:20:12 +0200

    Integrate with MongoDB stitch

--- app/src/main/java/za/co/dariel/washmycar/ui/ChooseActionFromMenuActivity.java
+++ app/src/main/java/za/co/dariel/washmycar/ui/ChooseActionFromMenuActivity.java
@@ -1,5 +1,6 @@
 package za.co.dariel.washmycar.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,8 +17,16 @@ public class ChooseActionFromMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_action_from_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
+        Button washMyCarButton = findViewById(R.id.wash_my_car);
+        washMyCarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/za/co/dariel/washmycar/ui/MainActivity.java
+++ app/src/main/java/za/co/dariel/washmycar/ui/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_action_from_menu);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
